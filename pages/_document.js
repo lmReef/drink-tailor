0eb6a63719cf3b4becda2ec7827d7d8787bfe999
Reef <reefmatson@gmail.com>
2022-01-21 18:08:53 +1300

    initial setup

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, { Html } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <Html>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+            <title>Drink Tailor</title>
+            <meta name="description" content="Cocktail recommender" />
+            <link rel="icon" href="/favicon.ico" />
+            <link
+              rel="preload"
+              href="https://fonts.googleapis.com/css?family=Poppins&display=optional"
+            />
+          </Html>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+// TODO: make dashboard page
+const Index = ({ theme }) => {
+  return (
+    <div className="main-wrapper">
+      <p>wehgrew</p>
+    </div>
+  );
+};
+
+export default Index;
