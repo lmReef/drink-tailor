5a1c1a7688eeecdeeebb76500d9f8e27c67d65cb
Reef <reefmatson@gmail.com>
2022-02-09 13:59:46 +1300

    added placeholder text for drinks, filters working
    - need to add details logic

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import Image from 'next/image';
 import {
   breakpoints_min,
   breakpoints_max,
@@ -26,9 +28,37 @@ const Content = styled.div`
   text-align: center;
   background-color: ${(props) => [props.theme[2]]};
   /* background-color: ${(props) => props.theme[0]}; */
+
+  .no-drinks {
+    position: relative;
+    width: 50%;
+    margin: auto;
+    top: 35%;
+    font-size: 3rem;
+  }
+
+  .drink-card {
+    margin: 1rem;
+
+    display: flex;
+
+    text-align: left;
+
+    img {
+      width: 300px;
+      height: 300px;
+      margin-right: 1rem;
+
+      border-radius: 10px;
+    }
+
+    h3 {
+      font-size: 1.3rem;
+    }
+  }
 `;
 
-const fetchCocktails = async (tags) => {
+const getDrinks = async (tags) => {
   const options = {
     method: 'GET',
     url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
@@ -44,22 +74,38 @@ const fetchCocktails = async (tags) => {
   return data.drinks;
 };
 
+const getDetails = async (id) => {
+  var options = {
+    method: 'GET',
+    url: 'https://the-cocktail-db.p.rapidapi.com/lookup.php',
+    params: { i: id },
+    headers: {
+      'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com',
+      'x-rapidapi-key': 'cd095dd235mshadd3945f9bfaac1p1347b1jsnb88073d8b4f0',
+    },
+  };
+
+  const { data } = await axios.request(options);
+
+  return data.drinks[0];
+};
+
 const Index = () => {
   // const theme = colors_light;
   const theme = colors_dark;
   const [timeoutId, setTimeoutId] = useState(null);
   const [data, setData] = useState(null);
+  const [hasTags, setHasTags] = useState(false);
 
   const handleTagChange = async (tags) => {
-    console.log('tags', tags);
     if (tags.length === 0) {
-      console.log('0');
       setData(null);
+      setHasTags(false);
       return;
     }
 
-    setData(await fetchCocktails(tags));
-    console.log('tags', tags);
+    setData(await getDrinks(tags));
+    setHasTags(true);
 
     // TODO: implement delay
     // if (timeoutId) clearTimeout(timeoutId);
@@ -73,17 +119,35 @@ const Index = () => {
     // );
   };
 
-  console.log('data', data);
-
   return (
     <Wrapper theme={theme}>
       <SideMenu theme={theme} handleTagChange={handleTagChange} />
       <Content theme={theme} className="main-wrapper">
-        {typeof data !== 'string' && data?.length > 0
-          ? data?.map((drink, index) => {
-              return <p key={index}>{drink.strDrink}</p>;
-            })
-          : ''}
+        {typeof data !== 'string' && data?.length > 0 ? (
+          data?.map((drink, index) => {
+            return (
+              <div key={index} className="drink-card">
+                <img
+                  src={drink.strDrinkThumb}
+                  alt={`Image of a ${drink.strDrink}`}
+                  className="card-image"
+                />
+                <div className="col">
+                  <h3>{drink.strDrink}</h3>
+                  {/* <button className="view-details">View Details</button> */}
+                </div>
+              </div>
+            );
+          })
+        ) : hasTags === false ? (
+          <h2 className="no-drinks">
+            Pick a few options on the left to get started.
+          </h2>
+        ) : (
+          <h2 className="no-drinks">
+            We dont know any drinks with that combination! Try something else.
+          </h2>
+        )}
       </Content>
     </Wrapper>
   );
