ba9733e093cc332d006105a37f56e87f12ec3bc9
Reef <reefmatson@gmail.com>
2022-03-17 16:19:35 +1300

    fix searchbar styling

--- components/top-bar/searchbar.tsx
+++ components/top-bar/searchbar.tsx
@@ -5,9 +5,15 @@ const StyledSearchbar = styled.input`
   width: 30rem;
 
   margin: 0.8rem 0 0.8rem 1rem;
+  padding: 1rem;
 
   background-color: ${colors.secondary};
   border-radius: 110px;
+  border: none;
+
+  :focus {
+    outline: none;
+  }
 `;
 
 const Searchbar = () => {
