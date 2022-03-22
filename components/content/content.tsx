5d527258396e3859d700f18183da0123ae95d039
Reef <reefmatson@gmail.com>
2022-03-22 13:35:04 +1300

    add scroll to top button

--- components/content/content.tsx
+++ components/content/content.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronCircleUp } from '@fortawesome/free-solid-svg-icons';
 
 import { colors } from '../../styles/theme';
 import { selectAllTags } from '../common/tags/tagSlice';
@@ -22,6 +24,25 @@ const StyledContent = styled.div`
     top: 35%;
     font-size: 3rem;
   }
+
+  .scroll-to-top {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin: 2.5rem 4rem;
+    cursor: pointer;
+
+    transition: transform 0.1s ease-in-out;
+
+    :hover {
+      transform: translateY(-0.3rem);
+    }
+
+    svg {
+      color: ${colors.accent};
+      font-size: 2.3rem;
+    }
+  }
 `;
 
 const Content = () => {
@@ -29,6 +50,8 @@ const Content = () => {
   const [drinks, setDrinks] = useState<DrinkBasic[]>([]);
   const [hasTags, setHasTags] = useState<boolean>(false);
   const contentRef = useRef<HTMLDivElement>();
+
+  const scrollToTop = () => contentRef.current.scrollTo(0, 0);
 
   useEffect(() => {
     const handleTagsChange = async () => {
@@ -45,7 +68,7 @@ const Content = () => {
       setDrinks(drinksRes);
       setHasTags(true);
 
-      contentRef.current.scrollTo(0, 0);
+      scrollToTop();
     };
 
     handleTagsChange();
@@ -66,6 +89,11 @@ const Content = () => {
           We dont know any drinks with that combination! Give something else a
           try.
         </h2>
+      )}
+      {hasTags && (
+        <div className="scroll-to-top" onClick={scrollToTop}>
+          <FontAwesomeIcon icon={faChevronCircleUp} />
+        </div>
       )}
     </StyledContent>
   );
