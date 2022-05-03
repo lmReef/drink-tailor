cc253e3854ff5d19b58495a9e3d122295e92beb1
Reef <reefmatson@gmail.com>
2022-05-04 10:05:31 +1200

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,8 @@ import Script from 'next/script';
 function App({ Component, pageProps }) {
   return (
     <>
+      {/* Google stuff */}
+      {/* Ads */}
       <Script
         id="Adsense-id"
         data-ad-client="ca-pub-1450000289109452"
@@ -15,6 +17,21 @@ function App({ Component, pageProps }) {
         strategy="beforeInteractive"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1450000289109452"
       />
+      {/* Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload" id="google-analytics-inline">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Provider store={store}>
         <CustomThemeProvider>
           <GlobalStyle />
