7dfe6f3a87c5b20f34e398d475863175df99153e
Reef <reefmatson@gmail.com>
2022-05-11 19:44:00 +1200

    disable user scale

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,10 +23,18 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
             <title>Drink Tailor</title>
             <meta name="description" content="Cocktail recommender" />
+            {/* disable user scale because we dont want it on mobile */}
+            {/* esp because input ele zoom */}
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, user-scalable=no"
+            />
+            {/* google verification */}
             <meta
               name="google-site-verification"
               content="4lu8PXHVMqY8XR3nnFwfuG8XULRJEO3PymODSnwfqM4"
             />
+
             <link rel="shortcut icon" href="/assets/images/favicon.ico" />
             <link
               rel="apple-touch-icon"
